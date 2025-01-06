9e119dd005962a69757c81313b11b9160798db38
ricardocr <ricardocribeiro@hotmail.com>
2025-01-06 18:45:57 -0300

    menu da agenda criado

---
+++ agenda.py
@@ -0,0 +1,40 @@
+while True:
+    print ("\nMenu do Gerenciador de Agenda 📑:\n")    
+    print ("1. Adicione um contado")
+    print ("2. Ver contatos")
+    print ("3. Editar Contato")
+    print ("4. Ver sua lista de Favoritos")
+    print ("5. Marcar um contato como Favorito")
+    print ("6. Desmarcar um contato como Favorito")
+    print ("7. Deletar contatos da agenda")
+    print ("8. Sair\n")
+
+    escolha = input ("Escolha uma opção, por favor: ")
+
+    if escolha == "1":
+        add_contato(agenda)
+    
+    elif escolha == "2":
+        ver_agenda(agenda)
+    
+    elif escolha == "3":
+        editar_agenda(agenda)
+    
+    elif escolha == "4":
+        ver_favoritos(agenda)
+    
+    elif escolha == "5":
+        marcar_favorito(agenda)
+
+    elif escolha == "6":
+        desmarcar_favorito(agenda)
+    
+    elif escolha == "7":
+        delete_contato(agenda)
+
+    elif escolha == "8":
+        print("\nPrograma finalizado. Até logo!")
+        break
+
+    else:
+        print ("Programa Agenda, finalizada")
